a32036863cf7bf881a931a11d44a2798e4c96b82
richard condori <dionirjf@gmail.com>
2020-07-20 21:59:23 -0400

    docs agregado

---
+++ docs/precache-manifest.707d3c49c9b8de92a6f2f5612fbf8f53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e2e4c8ce7f8242ca4e7b41b473cf44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4db805ba196106671087",
+    "url": "/static/css/main.a4aa3037.chunk.css"
+  },
+  {
+    "revision": "13fc4745cabd6b519965",
+    "url": "/static/js/2.b1b840ba.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.b1b840ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4db805ba196106671087",
+    "url": "/static/js/main.d1e821c3.chunk.js"
+  },
+  {
+    "revision": "69fd04938564b9a75943",
+    "url": "/static/js/runtime-main.b6f9b2a3.js"
+  }
+]);
